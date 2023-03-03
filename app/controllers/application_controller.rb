de4c303a9b3c4935db85eed72f2a91e12abc63f5
Mickey254-ja <mickey.awino@student.moringaschool.com>
2023-03-03 16:48:53 +0300

    post request for newuser account creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,14 @@ class ApplicationController < Sinatra::Base
     @message = {error: "Invalid username or password"}
     return @message.to_json
   end
+
+  # create a user account with username, password and email
+  post '/newuser' do
+    new_user = User.create(
+      username: params[:username],
+      email: params[:email],
+      password: params[:password]
+    )
+    new_user.to_json
+  end
 end
